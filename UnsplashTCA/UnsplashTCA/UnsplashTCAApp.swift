8faa5f874a134f639da17882d10fc50d444baae2
Fadeev Sergey <zashchta73@gmail.com>
2023-11-02 11:28:40 +0400

    empty project

---
+++ UnsplashTCA/UnsplashTCA/UnsplashTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashTCAApp.swift
+//  UnsplashTCA
+//
+//  Created by Sergey on 02.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
